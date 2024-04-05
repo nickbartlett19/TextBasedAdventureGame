4bfbde8cdefeb64b5b2b255ccd069d585d53400f
Nick Bartlett <nickbartlett19@gmail.com>
2024-04-05 15:26:37 -0400

    build ratchet prototype

--- script.js
+++ script.js
@@ -1,68 +1,58 @@
-const story001 = "There is a red door and a green door. Choose one to open."
-const story001_1 = "Red door."
-const story001_2 = "Green door."
-const story001_3 = "Neither."
-
 class GameStage {
-    constructor(storyCell, option1, option2, option3) {
-        this.text = storyCell;
+    constructor(storyObject) {
+        this.text = storyObject.body;
 
-        this.option1 = option1;
-        this.option2 = option2;
-        this.option3 = option3;
+        this.option1 = storyObject.opt1;
+        this.option2 = storyObject.opt2;
+        this.option3 = storyObject.opt3;
     }
 }
-
-// event listener on every option box for select() function
-
 
 // ========================================================
 
 // load HTML elements
-
 const screenBox = document.querySelector(".screen-box");
 const optionBoxes = Array.prototype.slice.call(document.querySelectorAll(".option-box"));
-console.log(typeof optionBoxes)
 
 function updateGameDisplay(gameStage, screenBox, optionBoxes) {
     screenBox.innerText = gameStage.text;
 
-    optionBoxes[0].innerText = gameStage.option1;
-    optionBoxes[1].innerText = gameStage.option2;
-    optionBoxes[2].innerText = gameStage.option3;
+    optionBoxes[0].innerText = gameStage.option1.body;
+    optionBoxes[1].innerText = gameStage.option2.body;
+    optionBoxes[2].innerText = gameStage.option3.body;
+
+    optionBoxes[0].data = gameStage.option1;
+    optionBoxes[1].data = gameStage.option2;
+    optionBoxes[2].data = gameStage.option3;
 
 }
 
-const firstGameStage = new GameStage(story001, story001_1, story001_2, story001_3);
 
+
+function gameStep(option) {
+    // console.log(option.data)
+    let storyData = option.data.ptr;
+    if (storyData === undefined) {
+        return;
+    }
+    // console.log(storyData);
+    currGameStage = new GameStage(storyData)
+    updateGameDisplay(currGameStage, screenBox, optionBoxes);
+
+}
+
+const firstGameStage = new GameStage(s001);
 updateGameDisplay(firstGameStage, screenBox, optionBoxes);
+
+optionBoxes.forEach((box) => {
+    box.addEventListener(('click'), (e) => gameStep(box))
+});
+
 
 // ========================================================
 
-// fetch("https://localhost:8080/story/001.txt")
-//   .then((res) => res.text())
-//   .then((text) => {
-//     console.log(text);
-//    })
-//   .catch((e) => console.error(e));
+// idea i had yesterday
 
-// class MainControl {
-//     constructor() {
-//         console.log("constructing...")
-//     }
-
-//     waitForChoice() {
-
-//     }
-
-//     updateScreenBox() {
-
-//     }
-
-//     nextGameStage() {
-
-//     }
-
-// }
-
-// let mc = new MainControl()
+// option button has event listener with gameStep() function
+// gameStep loads the gameStage corresponding to the pointer in that option
+// update display accordingly
